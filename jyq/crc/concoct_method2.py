0a35d805c48d1fe73368f13ac38a8af39b236acb
qiusuor <750435412@qq.com>
2017-12-12 22:23:49 +0800

    Update concoct_method2.py

--- jyq/crc/concoct_method2.py
+++ jyq/crc/concoct_method2.py
@@ -20,7 +20,7 @@ def check_and_delete(table):
     return finished
 
 if __name__=="__main__": 
-    cmd="cd contigs && bowtie2-build -f final.contigs.cut.500bp.95%.identity.fa final.contigs.cut.500bp.95%.identity.fa && cd .."
+    cmd="cd contigs && bowtie2-build -f *fasta final.contigs.cut.500bp.95%.identity.fa && cd .."
     print(cmd)
     subprocess.call(cmd,shell=True,env=new_env)
     print("Bowtie2-build finish")
@@ -44,7 +44,7 @@ if __name__=="__main__":
 #       print(source_1[i])
         sample_name='.'.join(source_1[i].split('.')[:3])
 #       print(sample_name) 
-        cmd="bowtie2 --no-mixed -f --fr -x contigs/final.contigs.cut.500bp.95%.identity.fa -1  "+"samples/"+sample_name+'/'+ source_1[i]+" -2 "+"samples/"+sample_name+'/'+source_2[i]+" -S "+sample_name+"_pair.sam -p 1"
+        cmd="bowtie2 --no-mixed -f --fr -x contigs/final.contigs.cut.500bp.95%.identity.fa -1  "+"samples/"+sample_name+'/'+ source_1[i]+" -2 "+"samples/"+sample_name+'/'+source_2[i]+" -S "+sample_name+"_pair.sam -p 1 && "+ "samtools view -b -S "+"samples/"+sample_name+'/'+sample_name+"_pair.sam -o "+"samples/"+sample_name+'/'+sample_name+"_pair.bam && "+"samtools sort -T "+"samples/"+sample_name+'/'+ " -o "+"samples/"+sample_name+'/'+sample_name+"_pair-smds.bam  "+"samples/"+sample_name+'/'+sample_name+"_pair.bam && "+"samtools index "+"samples/"+sample_name+'/'+ " -o "+"samples/"+sample_name+'/'+sample_name+"_pair-smds.bam && "+ "rm *_pair.sam && rm *_pair.bam "
         print(cmd)
         table.append(subprocess.Popen(cmd,shell=True,env=new_env))
     if(i<len(source_1)-1):
@@ -54,7 +54,7 @@ if __name__=="__main__":
             finished=check_and_delete(table)
             for j in range(i,min(i+finished,len(source_1))):
                 sample_name='.'.join(source_1[j].split('.')[:3])
-                cmd="bowtie2 --no-mixed -f --fr -x contigs/final.contigs.cut.500bp.95%.identity.fa -1  "+"samples/"+sample_name+'/'+ source_1[j]+" -2 "+"samples/"+sample_name+'/'+source_2[j]+" -S "+sample_name+"_pair.sam -p 1"
+                cmd="bowtie2 --no-mixed -f --fr -x contigs/final.contigs.cut.500bp.95%.identity.fa -1  "+"samples/"+sample_name+'/'+ source_1[j]+" -2 "+"samples/"+sample_name+'/'+source_2[j]+" -S "+sample_name+"_pair.sam -p 1 && "+"samtools view -b -S "+"samples/"+sample_name+'/'+sample_name+"_pair.sam -o "+"samples/"+sample_name+'/'+sample_name+"_pair.bam && "+ "samtools sort -T "+"samples/"+sample_name+'/'+ " -o "+"samples/"+sample_name+'/'+sample_name+"_pair-smds.bam  "+"samples/"+sample_name+'/'+sample_name+"_pair.bam && "+"samtools index "+"samples/"+sample_name+'/'+ " -o "+"samples/"+sample_name+'/'+sample_name+"_pair-smds.bam && " + "rm *_pair.sam && rm *_pair.bam "
                 print(cmd)
                 table.append(subprocess.Popen(cmd,shell=True,env=new_env))
             i=min(i+finished,len(source_1))
@@ -67,95 +67,95 @@ if __name__=="__main__":
             table[i].wait()
 
 
-    table=[]
-    i=0
-    for i in range(thread):
-#       print(source_1[i])
-        sample_name='.'.join(source_1[i].split('.')[:3])
-#       print(sample_name) 
-        cmd="samtools view -b -S "+"samples/"+sample_name+'/'+sample_name+"_pair.sam -o "+"samples/"+sample_name+'/'+sample_name+"_pair.bam"
-        print(cmd)
-        table.append(subprocess.Popen(cmd,shell=True,env=new_env))
-    if(i<len(source_1)-1):
-        i=thread    
-        while True:
-            time.sleep(1)
-            finished=check_and_delete(table)
-            for j in range(i,min(i+finished,len(source_1))):
-                sample_name='.'.join(source_1[j].split('.')[:3])
-                cmd="samtools view -b -S "+"samples/"+sample_name+'/'+sample_name+"_pair.sam -o "+"samples/"+sample_name+'/'+sample_name+"_pair.bam"
-                print(cmd)
-                table.append(subprocess.Popen(cmd,shell=True,env=new_env))
-            i=min(i+finished,len(source_1))
-            if(i==len(source_1)):
-                for x in table:
-                    x.wait()
-                break
-    else:
-        for i in range(len(source_1)):
-            table[i].wait()
+#     table=[]
+#     i=0
+#     for i in range(thread):
+# #       print(source_1[i])
+#         sample_name='.'.join(source_1[i].split('.')[:3])
+# #       print(sample_name) 
+#         cmd="samtools view -b -S "+"samples/"+sample_name+'/'+sample_name+"_pair.sam -o "+"samples/"+sample_name+'/'+sample_name+"_pair.bam"
+#         print(cmd)
+#         table.append(subprocess.Popen(cmd,shell=True,env=new_env))
+#     if(i<len(source_1)-1):
+#         i=thread    
+#         while True:
+#             time.sleep(1)
+#             finished=check_and_delete(table)
+#             for j in range(i,min(i+finished,len(source_1))):
+#                 sample_name='.'.join(source_1[j].split('.')[:3])
+#                 cmd="samtools view -b -S "+"samples/"+sample_name+'/'+sample_name+"_pair.sam -o "+"samples/"+sample_name+'/'+sample_name+"_pair.bam"
+#                 print(cmd)
+#                 table.append(subprocess.Popen(cmd,shell=True,env=new_env))
+#             i=min(i+finished,len(source_1))
+#             if(i==len(source_1)):
+#                 for x in table:
+#                     x.wait()
+#                 break
+#     else:
+#         for i in range(len(source_1)):
+#             table[i].wait()
 
 
-    table=[]
-    i=0
-    for i in range(thread):
-#       print(source_1[i])
-        sample_name='.'.join(source_1[i].split('.')[:3])
-#       print(sample_name) 
-        cmd="samtools sort -T "+"samples/"+sample_name+'/'+ " -o "+"samples/"+sample_name+'/'+sample_name+"_pair-smds.bam  "+"samples/"+sample_name+'/'+sample_name+"_pair.bam"
-        print(cmd)
-        table.append(subprocess.Popen(cmd,shell=True,env=new_env))
-    if(i<len(source_1)-1):
-        i=thread    
-        while True:
-            time.sleep(1)
-            finished=check_and_delete(table)
-            for j in range(i,min(i+finished,len(source_1))):
-                sample_name='.'.join(source_1[j].split('.')[:3])
-                cmd="samtools sort -T "+"samples/"+sample_name+'/'+ " -o "+"samples/"+sample_name+'/'+sample_name+"_pair-smds.bam  "+"samples/"+sample_name+'/'+sample_name+"_pair.bam"
-                print(cmd)
-                table.append(subprocess.Popen(cmd,shell=True,env=new_env))
-            i=min(i+finished,len(source_1))
-            if(i==len(source_1)):
-                for x in table:
-                    x.wait()
-                print("Map-bowtie2 finish")
-                break
-    else:
-        for i in range(len(source_1)):
-            table[i].wait()
-            print("Map-bowtie2 finish")
+#     table=[]
+#     i=0
+#     for i in range(thread):
+# #       print(source_1[i])
+#         sample_name='.'.join(source_1[i].split('.')[:3])
+# #       print(sample_name) 
+#         cmd="samtools sort -T "+"samples/"+sample_name+'/'+ " -o "+"samples/"+sample_name+'/'+sample_name+"_pair-smds.bam  "+"samples/"+sample_name+'/'+sample_name+"_pair.bam"
+#         print(cmd)
+#         table.append(subprocess.Popen(cmd,shell=True,env=new_env))
+#     if(i<len(source_1)-1):
+#         i=thread    
+#         while True:
+#             time.sleep(1)
+#             finished=check_and_delete(table)
+#             for j in range(i,min(i+finished,len(source_1))):
+#                 sample_name='.'.join(source_1[j].split('.')[:3])
+#                 cmd="samtools sort -T "+"samples/"+sample_name+'/'+ " -o "+"samples/"+sample_name+'/'+sample_name+"_pair-smds.bam  "+"samples/"+sample_name+'/'+sample_name+"_pair.bam"
+#                 print(cmd)
+#                 table.append(subprocess.Popen(cmd,shell=True,env=new_env))
+#             i=min(i+finished,len(source_1))
+#             if(i==len(source_1)):
+#                 for x in table:
+#                     x.wait()
+#                 print("Map-bowtie2 finish")
+#                 break
+#     else:
+#         for i in range(len(source_1)):
+#             table[i].wait()
+#             print("Map-bowtie2 finish")
 
 
-     table=[]
-    i=0
-    for i in range(thread):
-#       print(source_1[i])
-        sample_name='.'.join(source_1[i].split('.')[:3])
-#       print(sample_name) 
-        cmd="samtools index "+"samples/"+sample_name+'/'+ " -o "+"samples/"+sample_name+'/'+sample_name+"_pair-smds.bam"
-        print(cmd)
-        table.append(subprocess.Popen(cmd,shell=True,env=new_env))
-    if(i<len(source_1)-1):
-        i=thread    
-        while True:
-            time.sleep(1)
-            finished=check_and_delete(table)
-            for j in range(i,min(i+finished,len(source_1))):
-                sample_name='.'.join(source_1[j].split('.')[:3])
-                cmd="samtools index "+"samples/"+sample_name+'/'+ " -o "+"samples/"+sample_name+'/'+sample_name+"_pair-smds.bam"
-                print(cmd)
-                table.append(subprocess.Popen(cmd,shell=True,env=new_env))
-            i=min(i+finished,len(source_1))
-            if(i==len(source_1)):
-                for x in table:
-                    x.wait()
-                print("Map-bowtie2 finish")
-                break
-    else:
-        for i in range(len(source_1)):
-            table[i].wait()
-            print("Map-bowtie2 finish")
+#      table=[]
+#     i=0
+#     for i in range(thread):
+# #       print(source_1[i])
+#         sample_name='.'.join(source_1[i].split('.')[:3])
+# #       print(sample_name) 
+#         cmd="samtools index "+"samples/"+sample_name+'/'+ " -o "+"samples/"+sample_name+'/'+sample_name+"_pair-smds.bam"
+#         print(cmd)
+#         table.append(subprocess.Popen(cmd,shell=True,env=new_env))
+#     if(i<len(source_1)-1):
+#         i=thread    
+#         while True:
+#             time.sleep(1)
+#             finished=check_and_delete(table)
+#             for j in range(i,min(i+finished,len(source_1))):
+#                 sample_name='.'.join(source_1[j].split('.')[:3])
+#                 cmd="samtools index "+"samples/"+sample_name+'/'+ " -o "+"samples/"+sample_name+'/'+sample_name+"_pair-smds.bam"
+#                 print(cmd)
+#                 table.append(subprocess.Popen(cmd,shell=True,env=new_env))
+#             i=min(i+finished,len(source_1))
+#             if(i==len(source_1)):
+#                 for x in table:
+#                     x.wait()
+#                 print("Map-bowtie2 finish")
+#                 break
+#     else:
+#         for i in range(len(source_1)):
+#             table[i].wait()
+#             print("Map-bowtie2 finish")
 
 
 
@@ -165,7 +165,7 @@ if __name__=="__main__":
     cmd="rm -rf input && mkdir input && python /home/qiusuo/CONCOCT-master/scripts/gen_input_table.py contigs/final.contigs.cut.500bp.95%.identity.fa "
     for i in range(len(source_1)):
         sample_name='.'.join(source_1[i].split('.')[:3])
-        cmd=cmd+"samples/"+sample_name+"/"+sample_name+"_pair.bam "
+        cmd=cmd+"samples/"+sample_name+"/"+sample_name+"pair-smds.bam "
     cmd=cmd+"> input/cov_inputtableR.tsv"
     print(cmd)
     gen_input_table=subprocess.call(cmd,shell=True,env=new_env)
